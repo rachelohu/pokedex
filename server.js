51c5c9f564bea05344d856174d4665eec2a8f5d3
rachelohu <rachelohu5@gmail.com>
2021-09-29 01:30:32 -0400

    index and show routes are working

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
 const express = require('express');
 const app = express();
 const Pokemon = require('./models/pokemon.js');
+const methodOverride = require('method-Override');
+
+app.use(methodOverride("_method"));
 
 // Index Route
-app.get('/', (req,res) => {
+app.get('/pokemon', (req,res) => {
     res.render('index.ejs', {data: Pokemon});
-})
+});
 
 // New Route
-// app.get('/new', (req, res) => {
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs', {})
+});
+
+// Destroy Route
+// app.delete('/pokemon/:indexOfPokemonArray', (req, res) => {
+//     data.splice(req.params.indexOfPokemonArray, 1)
+//     res.redirect("/")
+// });
+
+// Update Route
+// app.get('/pokemon/new', (req, res) => {
 //     res.render('new.ejs', {})
-// })
+// });
+
+// Create Route
+// app.put('/pokemon/:indexOfPokemonArray', (req, res) => {
+//
+
+// Edit Route
+// app.get('/pokemon/:indexOfPokemonArray/edit', (req, res) => {
+//     res.render('edit.ejs', {
+//     Pokemon: })
+// });
 
 // Show Route
-app.get('/:id', (req, res) => {
+app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', {data: Pokemon[req.params.id]});
 });
 
 // Server Listener
 app.listen(3000, () => {
     console.log('listening')
-})
+});
