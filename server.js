6ad237d2caca9dbded96256d831404391be40c11
rachelohu <rachelohu5@gmail.com>
2021-10-02 18:33:07 -0400

    delete is working

--- server.js
+++ server.js
@@ -3,53 +3,47 @@ const app = express();
 const pokemon = require('./models/pokemon');
 const methodOverride = require('method-Override');
 
+app.use(express.urlencoded({extended: false}));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
 // Index Route
 app.get('/pokemon/', (req,res) => {
-        res.render('index.ejs', {data: pokemon, title:"Pokemon" });
+        res.render('index.ejs', {data: pokemon, title: "Pokemon - Index Page"});
 });
 
 // New Route
 app.get('/pokemon/new', (req, res) => {
-    res.render('new.ejs', {title: "Pokemon New Page"});
+    res.render('new.ejs', {title: "Pokemon - New Page"});
 });
 
-//Create Route
-app.post('/pokemon', (req, res) => {
-    const id = req.params.id;
-    data.create(pokemon[req.params.id], (error, createPokemon) => {
-        res.redirect("/pokemon")
-    } );
-} );
 
 // Delete Route
 app.delete('/pokemon/:id', (req, res) => {
-    const id = req.params.id;
-    data.findByIdAndDelete(pokemon[req.params.id], (error, deletePokemon)  => {
+    pokemon.splice(req.params.id, 1)
         res.redirect("/pokemon")
-    })
 });
 
 // Update Route
 app.put('/pokemon/:id', (req, res) => {
-    const id = req.params.id
-    data.findByIdAndUpdate
-    res.render('new.ejs', {data: pokemon[req.params.id]})
+    pokemon[req.params.id] = req.params.id
+    res.render('/pokemon')
+});
+
+//Create Route
+app.post('/pokemon', (req, res) => {
+    pokemon.push(req.params.id)
+    res.redirect("/pokemon")
 });
 
 // Edit Route
 app.get('/pokemon/:id/edit', (req, res) => {
-     const id = req.params.id
-     data.findById(id, (error, pokemon) => {
-    res.render('edit.ejs', {data: pokemon[req.params.id]})
-    })
+    res.render('edit.ejs', {data: pokemon[req.params.id], title: "Pokemon - Edit Page"})
 });
 
 // Show Route
 app.get('/pokemon/:id', (req, res) => {
-    res.render('show.ejs', {data: pokemon[req.params.id]});
+    res.render('show.ejs', {data: pokemon[req.params.id], title: "Pokemon - Show Page"});
 });
 
 // Server Listener
