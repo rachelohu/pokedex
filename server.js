53965278f669df86401d13e8f08aa35053113232
rachelohu <rachelohu5@gmail.com>
2021-10-02 16:53:12 -0400

    made changes to delete mongoDB

--- server.js
+++ server.js
@@ -1,84 +1,55 @@
 const express = require('express');
 const app = express();
-const Pokemon = require('./models/pokemon.js');
-const pokemonList = require('./models/pokemon-list');
+const pokemon = require('./models/pokemon');
 const methodOverride = require('method-Override');
-
-const mongoose = require("mongoose");
-
-const DATABASE_URL = "mongodb+srv://sei:helloworld@sei.wjl9d.mongodb.net/pokedex?retryWrites=true&w=majority"
-
-const db=mongoose.connection;
-
-// Connect to the mongo database:
-mongoose.connect(DATABASE_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-
-// Database Connection Error/Success
-// Define callback functions for various events
-db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
-db.on('connected', () => console.log('mongo connected'));
-db.on('disconnected', () => console.log('mongo disconnected'));
 
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
 // Index Route
-app.get('/pokemon', (req,res) => {
-    Pokemon.find({}, (error, foundPokemon) => {
-        console.log( foundPokemon );
-        res.render('index.ejs', {
-            data: foundPokemon
-        });
-      });
+app.get('/pokemon/', (req,res) => {
+        res.render('index.ejs', {data: pokemon, title:"Pokemon" });
 });
 
 // New Route
 app.get('/pokemon/new', (req, res) => {
-    res.render('new.ejs', {data: pokemonList});
+    res.render('new.ejs', {title: "Pokemon New Page"});
 });
 
-app.get('/pokemon/new/:id', (req, res) => {
+//Create Route
+app.post('/pokemon', (req, res) => {
     const id = req.params.id;
-    const foundPokemon = pokemonList.find( pokemon => pokemon.id === id );
-    
-    Pokemon.create( foundPokemon, (error, createdPokemon) => {
+    data.create(pokemon[req.params.id], (error, createPokemon) => {
         res.redirect("/pokemon")
     } );
 } );
 
-// Destroy Route
+// Delete Route
 app.delete('/pokemon/:id', (req, res) => {
-    const id = req.params.id
-    Data.findByIdAndRemove(id, (err, Pokemon)  => {
+    const id = req.params.id;
+    data.findByIdAndDelete(pokemon[req.params.id], (error, deletePokemon)  => {
         res.redirect("/pokemon")
     })
 });
 
 // Update Route
-// app.get('/pokemon/:id', (req, res) => {
-//     const id = req.params.id
-//     Data.findByIdAndUpdate
-//     res.render('new.ejs', {})
-// });
-
-// Create Route
-// app.put('/pokemon/:id', (req, res) => {
-//
+app.put('/pokemon/:id', (req, res) => {
+    const id = req.params.id
+    data.findByIdAndUpdate
+    res.render('new.ejs', {data: pokemon[req.params.id]})
+});
 
 // Edit Route
 app.get('/pokemon/:id/edit', (req, res) => {
      const id = req.params.id
-     data.findById(id, (err, Pokemon) => {
-    res.render('edit.ejs', {data: pokemonList[req.params.id]})
+     data.findById(id, (error, pokemon) => {
+    res.render('edit.ejs', {data: pokemon[req.params.id]})
     })
 });
 
 // Show Route
 app.get('/pokemon/:id', (req, res) => {
-    res.render('show.ejs', {data: pokemonList[req.params.id]});
+    res.render('show.ejs', {data: pokemon[req.params.id]});
 });
 
 // Server Listener
